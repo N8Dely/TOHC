0d868254cf6e6976df77a430617551c1757199a4
Nathan Delahanty <NDelahanty94@gmail.com>
2021-11-16 12:29:38 -0500

    drop down navigation button and button styling

--- TOHC.js
+++ TOHC.js
@@ -1,14 +1,33 @@
 //Dark Mode by Matthew Marquise//
+
 function darkmode() {
-    const wasDarkmode = localStorage.getItem('darkmode') === 'true';
-    localStorage.setItem('darkmode', !wasDarkmode);
-    const element = document.body;
-    element.classList.toggle('dark-mode', !wasDarkmode);
-  }
+  const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+  localStorage.setItem('darkmode', !wasDarkmode);
+  const element = document.body;
+  element.classList.toggle('dark-mode', !wasDarkmode);
+}
 
-  function onload() {
-    document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
-    onload= "onlaod()"
-  }
+function onload() {
+  document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+  onload = "onlaod()"
+}
+//Dark Mode by Matthew Marquise//
 
-  //Dark Mode by Matthew Marquise//
+//Drop down navigation w3schools//
+
+function dropnavfunc() {
+  document.getElementById("tohc-drop-nav").classList.toggle("open");
+}
+window.onclick = function (event) {
+  if (!event.target.matches('.navdrop')) {
+    var navdrops = document.getElementsByClassName("drop-links");
+    var i;
+    for (i = 0; i < navdrops.length; i++) {
+      var openNavigation = navdrops[i];
+      if (openNavigation.classList.contains('open')) {
+        openNavigation.classList.remove('open');
+      }
+    }
+  }
+}
+  //Drop down navigation w3schools//
