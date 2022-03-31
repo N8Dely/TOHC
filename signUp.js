d88a849ee16dbeca96594cb9be9fe6db18cf434c
Nathan Delahanty <NDelahanty94@gmail.com>
2022-03-31 15:55:38 -0400

    word counter

--- signUp.js
+++ signUp.js
@@ -5,9 +5,10 @@ let saveFile = () => {
     const email = document.getElementById("txtEmail");
     const country = document.getElementById("selCountry");
     const msg = document.getElementById("msg");
+    const counter = document.getElementById("counter");
 
     // This variable stores all the data.
-    let data = "\r Name: " + name.value + " \r\n " + "Age: " + age.value + " \r\n " + "Email: " + email.value + " \r\n " + "Country: " + country.value + " \r\n " + "Message: " + msg.value;
+    let data = "\r Name: " + name.value + " \r\n " + "Age: " + age.value + " \r\n " + "Email: " + email.value + " \r\n " + "Country: " + country.value + " \r\n " + "Message: " + msg.value + " \r\n " + "Word Count: " + counter;
     console.log(data); //printing form data into the console
     // Convert the text to BLOB.
     const textToBLOB = new Blob([data], { type: "text/plain" });
@@ -37,3 +38,38 @@ let saveFile = () => {
 
     newLink.click();
 };
+
+
+//word counter
+counter = function() {
+    var value = $('#text').val();
+
+    if (value.length == 0) {
+        $('#wordCount').html(0);
+        $('#totalChars').html(0);
+        $('#charCount').html(0);
+        $('#charCountNoSpace').html(0);
+        return;
+    }
+
+    var regex = /\s+/gi;
+    var wordCount = value.trim().replace(regex, ' ').split(' ').length;
+    var totalChars = value.length;
+    var charCount = value.trim().length;
+    var charCountNoSpace = value.replace(regex, '').length;
+
+    $('#wordCount').html(wordCount);
+    $('#totalChars').html(totalChars);
+    $('#charCount').html(charCount);
+    $('#charCountNoSpace').html(charCountNoSpace);
+};
+
+$(document).ready(function() {
+    $('#count').click(counter);
+    $('#text').change(counter);
+    $('#text').keydown(counter);
+    $('#text').keypress(counter);
+    $('#text').keyup(counter);
+    $('#text').blur(counter);
+    $('#text').focus(counter);
+});
